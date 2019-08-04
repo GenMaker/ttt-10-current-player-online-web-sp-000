868c96dd7ba6d885c72a448cda52efc12a344b8a
Panela Leung <leungpanela@gmail.com>
2019-08-04 19:09:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,9 +4,12 @@ board = ["X"," "," ","0"," "," "," "," ","X"]
 def turn_count (board)
   counter = 0
   board.each do |turn|
-    if turn == "X" || turn == "O"
+    if turn == "X"
       counter += 1
       end
+      if turn == "O"
+        counter += 1
+        end
   end
 return counter
   end
