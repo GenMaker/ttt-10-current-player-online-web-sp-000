c978de72c80a439fa3edc6a59de3c10b84adba74
Panela Leung <leungpanela@gmail.com>
2019-08-04 19:03:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,12 +3,10 @@ board = ["X"," "," ","0"," "," "," "," ","X"]
 # create turn counter
 def turn_count (board)
   counter = 0
-while counter <=9
   board.each do |turn|
     if turn == "X" || turn == "O"
       counter += 1
-      end
       puts counter
-    end
+      end
   end
   end
